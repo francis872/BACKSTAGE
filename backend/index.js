e89bee83c92aacc831ed5950d3748735da9e78f8
francis872 <franciscogloopez@gmail.com>
2026-09-19 13:19:15 -0500

    feat: register operational events API

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ const territorialRoutes = require('./routes/territorial.routes');
 const usersRoutes = require('./routes/users.routes');
 const layersRoutes = require('./routes/layers.routes');
 const analysisRoutes = require('./routes/analysis.routes');
+const operationalEventsRoutes = require('./routes/operationalEvents.routes');
 const auditLogsRoutes = require('./routes/auditLogs.routes');
 const analyticsRoutes = require('./routes/analytics.routes');
 const { getExampleRecommendation } = require('./controllers/recommendations.controller');
@@ -87,6 +88,7 @@ app.use('/territorial', territorialRoutes);
 app.use('/users', usersRoutes);
 app.use('/layers', layersRoutes);
 app.use('/analysis', analysisRoutes);
+app.use('/operational-events', operationalEventsRoutes);
 app.use('/analytics', analyticsRoutes);
 app.use('/audit-logs', auditLogsRoutes);
 
